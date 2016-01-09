bead20e09e3ad1de7278b4f158e7d0bccde32214
John Sylvain <jsylvain007@gmail.com>
2016-01-08 22:01:49 -0600

    layout and terminal functionality

--- js/app.js
+++ js/app.js
@@ -1,10 +1,19 @@
-var JSONModel = {
+var model = {
+  previousCommands: [],
+  commandPrefix: '$',
+  currentOutput: null,
+  commands:[
+    '','open', 'open resume', 'show education','show xp', 'help'
+  ],
+  defaultMessage: {
+    message: "to view the resume, enter a command in the terminal to the left"
+  },
   data: {
     name: "John Sylvain",
     position: "Web developer and designer",
     contact:{
       email: "me@johnsylva.in",
-      phone: "(313) 618.0632"
+      phone: 3136180632
     },
     education: {
       name: 'Purdue University',
@@ -31,28 +40,14 @@ var JSONModel = {
 
 }
 
-var controller = {
-  init: function(){
-    resumeContentView.init();
-  },
-  getResumeData: function(){
-    return JSONModel.data;
-  }
-}
-
-var resumeContentView = {
-  init: function(){
-    this.resumeContainerElem = document.getElementById('resume-code');
-    this.render();
-  },
-
-  render: function(){
-    function JSONHighlight(json) {
-      if (typeof json != 'string') {
-       json = JSON.stringify(json, undefined, 2);
-     }
-     json = json.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;');
-     return json.replace(/("(\\u[a-zA-Z0-9]{4}|\\[^u]|[^\\"])*"(\s*:)?|\b(true|false|null)\b|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?)/g, function (match) {
+var filters = {
+  textToJSON: function(json){
+    if (typeof json != 'string') {
+      json = JSON.stringify(json, undefined, 2);
+    }
+    json = json.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;');
+    return json.replace(/("(\\u[a-zA-Z0-9]{4}|\\[^u]|[^\\"])*"(\s*:)?|\b(true|false|null)\b|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?)/g, 
+      function (match) {
       var cls = 'number';
       if (/^"/.test(match)) {
         if (/:$/.test(match)) {
@@ -67,43 +62,54 @@ var resumeContentView = {
       }
       return '<span class="' + cls + '">' + match + '</span>';
     });
-   }
-    var data = controller.getResumeData();
-    var json = JSONHighlight(JSON.stringify(data,null,'  '));
-    this.resumeContainerElem.innerHTML = json;
   }
 }
-controller.init();
 
-var consoleModel = {
-  previousCommands: [],
-  commandPrefix: '$',
-  commands:[
-    '', 'get', 'help'
-  ]
-}
-
-var consoleController = {
+var controller = {
   init: function(){
+    model.currentOutput = model.defaultMessage;
+    resumeContentView.init();
     consoleView.init();
+
+  },
+
+  getResumeData: function(){
+    return model.data;
+  },
+
+  getDefaultData: function(){
+    return model.defaultMessage;
+  },
+
+  getCurrentOutput: function(){
+    return model.currentOutput;
+  },
+
+  updateOutput: function(newOutput, callback){
+    model.currentOutput = newOutput;
+    callback();
   },
 
   enterCommand: function(command){
     var flag = false;
-    consoleModel.previousCommands.push({
+    model.previousCommands.push({
       text: command,
       type: 'command'
     });
-    for (var i = 0; i < consoleModel.commands.length; i++) {
-      var modelCommand = consoleModel.commands[i];
+    for (var i = 0; i < model.commands.length; i++) {
+      var modelCommand = model.commands[i];
       if (command === modelCommand){
         flag = true;
       }
     };
     if (!flag) {
-      consoleModel.previousCommands.push({
+      model.previousCommands.push({
         text: 'command not found: ' + command,
         type: 'error'
+      });
+      model.previousCommands.push({
+        text: 'to view available commands type: help',
+        type: 'response'
       });
     } else {
       this.executeCommand(command);
@@ -112,28 +118,48 @@ var consoleController = {
   },
 
   executeCommand: function(command){
+    var _this = this;
     var commands = {
       help: function(){
-        var commands = consoleModel.commands;
-        consoleModel.previousCommands.push({
+        var commands = model.commands;
+        model.previousCommands.push({
           text: 'Available Commands:',
           type: 'response'
         });
         for (var i = 0; i < commands.length; i++) {
           var avalCommand = commands[i];
-          consoleModel.previousCommands.push({
+          model.previousCommands.push({
             text: avalCommand,
             type: 'response'
           })
         };
         consoleView.render();
+      },
+      open: function(){
+        _this.updateOutput(model.data, function(){
+          resumeContentView.render();
+        });
       }
     }
     if(command !== '') commands[command]();
   },
 
   getPreviousCommands: function(){
-    return consoleModel.previousCommands;
+    return model.previousCommands;
+  }
+
+}
+
+var resumeContentView = {
+  init: function(){
+    this.resumeContainerElem = document.getElementById('resume-code');
+    this.render();
+  },
+
+  render: function(){
+    var data = controller.getCurrentOutput();
+    var json = filters.textToJSON(JSON.stringify(data,null,'  '));
+    this.resumeContainerElem.innerHTML = json;
   }
 }
 
@@ -141,24 +167,27 @@ var consoleView = {
   init: function(){
     this.promptElem = document.getElementById('command-prompt');
     this.prevElem = document.getElementById('commands');
+
+    var commandInput = document.getElementById('command-input');
+    commandInput.focus();
+
     this.promptElem.addEventListener('submit', function(e){
       e.preventDefault();
-      console.log(e.target.prompt.value);
       var command = e.target.prompt.value;
       e.target.prompt.value = '';
-      consoleController.enterCommand(command);
+      controller.enterCommand(command);
     })
     this.render();
   },
 
   render: function(){
     this.prevElem.innerHTML = '';
-    var commands = consoleController.getPreviousCommands();
+    var commands = controller.getPreviousCommands();
 
     for (var i = 0; i < commands.length; i++) {
       var command = commands[i];
-      console.log(command);
       var elem = document.createElement('li');
+
       if (command.type === 'command') {
         elem.textContent = '$ ' + command.text;
       } else if(command.type === 'error'){
@@ -175,4 +204,4 @@ var consoleView = {
 
   }
 }
-consoleController.init();
+controller.init();
