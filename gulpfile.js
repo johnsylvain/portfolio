fdc4e43fbe18ddf30fc47a722bf51534a55737f3
John Sylvain <john.n.sylvain@gmail.com>
2018-01-13 14:48:46 -0600

    uglify bundle

--- gulpfile.js
+++ gulpfile.js
@@ -30,9 +30,7 @@ gulp.task('js', () => {
     })
     .pipe(source('bundle.js'))
     .pipe(buffer())
-    // .pipe(gulpif(config.sourceMaps, sourcemaps.init()))
-      // .pipe(uglify())
-    // .pipe(gulpif(config.sourceMaps, sourcemaps.write()))
+    .pipe(uglify())
     .pipe(gulp.dest('./dist/js/'));
 });
 
